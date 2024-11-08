52c2a58ce3a96748ed0435d737cf6d7a9d867737
tzi-shue <93068847+tzi-shue@users.noreply.github.com>
2024-11-08 13:05:49 +0800

    Update frp.sh

--- frp.sh
+++ frp.sh
@@ -19,14 +19,14 @@ FRP_PATH=/usr/local/frp
 PROXY_URL="https://ghp.ci/"
 
 # check frpc
-if [ -f "/usr/local/frp/${FRP_NAME}" ] || [ -f "/usr/local/frp/${FRP_NAME}.toml" ] || [ -f "/lib/systemd/system/${FRP_NAME}.service" ];then
+if [ -f "/usr/local/frp/${FRP_NAME}" ] || [ -f "/usr/local/frp/${FRP_NAME}.ini" ] || [ -f "/lib/systemd/system/${FRP_NAME}.service" ];then
     echo -e "${Green}=========================================================================${Font}"
     echo -e "${RedBG}当前已退出脚本.${Font}"
     echo -e "${Green}检查到服务器已安装${Font} ${Red}${FRP_NAME}${Font}"
-    echo -e "${Green}请手动确认和删除${Font} ${Red}/usr/local/frp/${Font} ${Green}目录下的${Font} ${Red}${FRP_NAME}${Font} ${Green}和${Font} ${Red}/${FRP_NAME}.toml${Font} ${Green}文件以及${Font} ${Red}/lib/systemd/system/${FRP_NAME}.service${Font} ${Green}文件,再次执行本脚本.${Font}"
+    echo -e "${Green}请手动确认和删除${Font} ${Red}/usr/local/frp/${Font} ${Green}目录下的${Font} ${Red}${FRP_NAME}${Font} ${Green}和${Font} ${Red}/${FRP_NAME}.ini${Font} ${Green}文件以及${Font} ${Red}/lib/systemd/system/${FRP_NAME}.service${Font} ${Green}文件,再次执行本脚本.${Font}"
     echo -e "${Green}参考命令如下:${Font}"
     echo -e "${Red}rm -rf /usr/local/frp/${FRP_NAME}${Font}"
-    echo -e "${Red}rm -rf /usr/local/frp/${FRP_NAME}.toml${Font}"
+    echo -e "${Red}rm -rf /usr/local/frp/${FRP_NAME}.ini${Font}"
     echo -e "${Red}rm -rf /lib/systemd/system/${FRP_NAME}.service${Font}"
     echo -e "${Green}=========================================================================${Font}"
     exit 0
@@ -91,7 +91,7 @@ tar -zxvf ${FILE_NAME}.tar.gz
 mkdir -p ${FRP_PATH}
 mv ${FILE_NAME}/${FRP_NAME} ${FRP_PATH}
 
-# configure frpc.toml
+# configure frpc.ini
 # 生成随机 6 位字母和数字的服务名
 SERVICE_NAME=$(tr -dc 'a-zA-Z0-9' < /dev/urandom | head -c 6)
 # FRP 配置文件路径
@@ -131,7 +131,7 @@ Wants=network.target
 Type=simple
 Restart=on-failure
 RestartSec=5s
-ExecStart=/usr/local/frp/${FRP_NAME} -c /usr/local/frp/${FRP_NAME}.toml
+ExecStart=/usr/local/frp/${FRP_NAME} -c /usr/local/frp/${FRP_NAME}.ini
 
 [Install]
 WantedBy=multi-user.target
@@ -146,8 +146,8 @@ sudo systemctl enable ${FRP_NAME}
 rm -rf ${WORK_PATH}/${FILE_NAME}.tar.gz ${WORK_PATH}/${FILE_NAME} ${FRP_NAME}_linux_install.sh
 
 echo -e "${Green}====================================================================${Font}"
-echo -e "${Green}安装成功,请先修改 ${FRP_NAME}.toml 文件,确保格式及配置正确无误!${Font}"
-echo -e "${Red}vi /usr/local/frp/${FRP_NAME}.toml${Font}"
+echo -e "${Green}安装成功,请先修改 ${FRP_NAME}.ini 文件,确保格式及配置正确无误!${Font}"
+echo -e "${Red}vi /usr/local/frp/${FRP_NAME}.ini${Font}"
 echo -e "${Green}修改完毕后执行以下命令重启服务:${Font}"
 echo -e "${Red}sudo systemctl restart ${FRP_NAME}${Font}"
 echo -e "${Green}====================================================================${Font}"
