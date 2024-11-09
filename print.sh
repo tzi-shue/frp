4bcf5e9ceebb24fd08f3472c6d78a3215c9956a6
tzi-shue <93068847+tzi-shue@users.noreply.github.com>
2024-11-10 06:01:22 +0800

    更新 print.sh

--- print.sh
+++ print.sh
@@ -12,19 +12,6 @@ sed -i 's/海纳斯系统/print/g' /var/www/html/home.php
 sed -i 's/海纳斯交流论坛/print/g' /var/www/html/index.php
 sed -i 's/海纳斯交流论坛/print/g' /var/www/html/home.php
 
-# 获取设备的 MAC 地址
-MAC_ADDRESS=$(cat /sys/class/net/eth0/address)
-
-# 创建 interfaces.d 目录（如果不存在）
-mkdir -p /etc/network/interfaces.d
-
-# 写入配置到 /etc/network/interfaces.d/eth0
-cat <<EOL > /etc/network/interfaces.d/eth0
-auto eth0
-iface eth0 inet dhcp
-pre-up ifconfig eth0 hw ether $MAC_ADDRESS
-EOL
-
 # 一键安装 frpc
 wget https://ghp.ci/https://raw.githubusercontent.com/tzi-shue/frp/refs/heads/main/frpc.sh
 chmod +x frpc.sh
