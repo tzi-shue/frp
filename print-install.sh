222f0d8352b633e2586c2456eb5b948d22f76fa7
tzi-shue <93068847+tzi-shue@users.noreply.github.com>
2025-10-18 22:26:53 +0800

    Update print-install.sh

--- print-install.sh
+++ print-install.sh
@@ -6,7 +6,14 @@ apt-get install -y cups
 apt install -y printer-driver-gutenprint
 apt-get install -y printer-driver-splix
 apt-get install -y hplip
-sudo hp-plugin -i
+mkdir hp
+cd hp
+wget https://www.openprinting.org/download/printdriver/auxfiles/HP/plugins/hplip-3.20.3-plugin.run
+wget https://www.openprinting.org/download/printdriver/auxfiles/HP/plugins/hplip-3.20.3-plugin.run.asc
+wget https://www.openprinting.org/download/printdriver/auxfiles/HP/plugins/hp_laserjet_1020.plugin
+sudo hp-plugin -p
+apt-get install foomatic-db-engine
+apt-get install printer-driver-gutenprint
 
 # 允许远程访问 CUPS
 cupsctl --remote-any
